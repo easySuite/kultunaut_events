0678ae649b9362d12c886a6855f0e3f5bb82ad6c
Martin Cording <guddo@me.com>
2015-04-10 12:38:30 +0200

    Hotfix: Adjusted to changed Nodelist styles.

--- templates/kultunaut_events.event.carousel.tpl.php
+++ templates/kultunaut_events.event.carousel.tpl.php
@@ -23,8 +23,9 @@ $image = $image ? theme(
   <div class="item_content">
     <div class="expand"><?php echo $link; ?></div>
     <div class="label"><?php echo t('Calendar');?></div>
-    <div class="event-date">
-      <div class="event-day"><?php echo format_date($event_date, 'day_only'); ?></div>
+    <div class="event-time">
+      <div class="event-day"><?php echo $time; ?></div>
+      <div class="event-date"><?php echo format_date($event_date, 'day_only'); ?></div>
       <div class="event-month"><?php echo format_date($event_date, 'short_month_only'); ?></div>
     </div>
     <div class="image">
@@ -32,13 +33,7 @@ $image = $image ? theme(
     </div>
     <div class="data">
       <div class="caption">
-        <h3><?php echo $link;?></h3>
-      </div>
-      <div class="library">
-        <div class="event-time">
-          <span><?php echo t('Time');?></span>
-          <span><?php echo $time; ?></span>
-        </div>
+        <h3 style="height: 60px; max-height: 60px; overflow: hidden;"><?php echo $link;?></h3>
       </div>
     </div>
   </div>
